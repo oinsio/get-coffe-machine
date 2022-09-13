6ae1a4f01b9ad9b1d1a915527c0e638bac08ffd4
oinsio <oinsio.srv@gmail.com>
2022-09-13 17:31:49 +0600

    Change programmerPaths to static method

--- src/main/java/cto/fantask/PathToCoffeeMachine.java
+++ src/main/java/cto/fantask/PathToCoffeeMachine.java
@@ -2,7 +2,7 @@ package cto.fantask;
 
 public class PathToCoffeeMachine {
 
-    public int programmerPaths(int[][] grid) {
+    public static int programmerPaths(int[][] grid) {
 
         return 0;
     }
--- src/test/groovy/cto/fantask/PathToCoffeeMachineTest.groovy
+++ src/test/groovy/cto/fantask/PathToCoffeeMachineTest.groovy
@@ -1,18 +1,21 @@
 package cto.fantask
 
-import spock.lang.Shared
 import spock.lang.Specification
 
 class PathToCoffeeMachineTest extends Specification {
-
-    @Shared
-    def underTest = new PathToCoffeeMachine()
 
     def "should return 0 path"() {
         given:
             int[][] grid = [[0, 2] [1, 0]]
         expect:
-            underTest.programmerPaths(grid) == 0
+            PathToCoffeeMachine.programmerPaths(grid) == 0
+    }
+
+    def "should return 2 paths"() {
+        given:
+            int[][] grid = [[1, 0, 0, 0] [0, 0, 0, 0] [0, 0, 2, -1]]
+        expect:
+            PathToCoffeeMachine.programmerPaths(grid) == 2
     }
 
 }
